98698fcfadc3705179ddda5d8eedfc8155eadcd9
Edine <152820927+HoussemDhiaEddine19@users.noreply.github.com>
2025-12-12 06:34:37 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/sessiontoken/main.go
@@ -0,0 +1,23 @@
+// cmd/sessiontoken/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "sessiontoken/internal/sessiontoken"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := sessiontoken.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
